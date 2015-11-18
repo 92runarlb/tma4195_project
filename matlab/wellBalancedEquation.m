c6cc9f904c591402a15ec793bcaab8327d82298e
Runar Berge <runarlb@stud.ntnu.no>
2015-11-18 17:01:52 +0100

    Have no idea what is differetn

--- matlab/wellBalancedEquation.m
+++ matlab/wellBalancedEquation.m
@@ -3,25 +3,29 @@ clc; clear; close all;
 g = 9.81;
 
 
-xstart=-1000;
-xend = 1000;
+xstart=-1.5e4;
+xend = 1e4;
 
-T = 1;
-dt = 0.00001;
-dx = 0.1;
+T = 10*60;
+dt = 0.00005;
+dx = 1;
 
-epsilon = 1e2;
+epsilon = 5e4;
+waveHeight = 40;
 seaLevel = 0;
+plotCenter = 0;
 u_0 = @(x) 0*ones(size(x,1),1);
 %z_0 = @(x) min([(x-2),ones(size(x,1),1)],[],2);
 z_0 = @(x) 500/xend*(x-xend);
-h_0 = @(x) seaLevel*ones(size(x,1),1)+ 1*exp(-x.^2/epsilon)-min([z_0(x),seaLevel*ones(size(x))],[],2)+ 0.001;
+h_0 = @(x) seaLevel*ones(size(x,1),1)+ waveHeight*exp(-x.^2/epsilon)-min([z_0(x),seaLevel*ones(size(x))],[],2)+ 0.001;
 
 fluxW = @(h1,u1,h2,u2,lambda) 0.5*(h1.*u1+h2.*u2) %-1./(2*lambda).*(h1-h2);
 fluxZ = @(h1,u1,h2,u2,lambda) 0.5*(h1.*u1.^2 + g*h1.^2/2 + h2.*u2.^2 + g*h2.^2/2) ...
                             %-1./(20*lambda).*(h1.*u1-h2.*u2);
 
 x = (xstart:dx:xend)';
+xLessThanZero = x<0;
+startIndex = sum(xLessThanZero);
 z = z_0(x);
 u = u_0(x);
 h = h_0(x);
@@ -31,8 +35,10 @@ Z = h.*u;
 
 F = [];
 f = figure('units','normalized','outerposition',[0 0 1 1]);
-takePic = 100;
-i = 0;
+
+takePicture = 0:0.5:T;
+takePicIndex = 1;
+etaPic = [];
 for t= 0:dt:T
     zExt = [z(1); z; z(end)];
     hExt = [h(1); h; h(end)];
@@ -60,22 +66,32 @@ for t= 0:dt:T
     h = W;
     u = Z./h;
     
+    % Kill everything not wanted
     clf
     plot(x, h+z)
+    [~, plotCenter] = max(h(~xLessThanZero)+z(~xLessThanZero));
+    plotCenter = x(plotCenter+startIndex);
     hold on
     plot(x,z);
-    axis([0-50,xend,seaLevel-1,seaLevel+1])
+    axis([plotCenter-500,plotCenter+500,seaLevel-waveHeight,seaLevel+waveHeight])
     title(t)
-    if mod(i,takePic)==0
-        F = [F, getframe(f)];    
+    pause(0.0000000000000001)
+    if t>=takePicture(takePicIndex)
+        etaPic = [etaPic, h + z];
+        takePicIndex = takePicIndex + 1
     end
+    
+    if plotCenter>xend - 200
+        break
+    end
+    
 end
 
-close all
-
-name = strcat('shallowWaterChangingBottom',num2str(xstart),'_',num2str(xend),'_',num2str(T),'_','.avi');
-myVid = VideoWriter(name);
-myVid.FrameRate = round(24/dt);
-open(myVid);
-writeVideo(myVid, F);
-close(myVid)
+% close all
+% 
+% name = strcat('shallowWaterChangingBottom',num2str(xstart),'_',num2str(xend),'_',num2str(T),'_','.avi');
+% myVid = VideoWriter(name);
+% myVid.FrameRate = round(24);
+% open(myVid);
+% writeVideo(myVid, F);
+% close(myVid)
